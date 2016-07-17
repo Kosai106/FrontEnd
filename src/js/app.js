4f7b3681e27c079256bc3d5904c8b9dc1f4c5e81
Kevin Østerkilde <kevin@oesterkilde.dk>
2016-07-17 16:35:06 +0200

    Added node-sass for automatic SCSS compiles

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import ReactDOM     from 'react-dom';
 import Navigation   from './components/navigation/navigation';
 import Footer       from './components/footer/footer';
 import TrainersList from './components/trainers/trainersList';
+require('../scss/style.scss');
 
 const nav    = document.getElementById('nav');
 const app    = document.getElementById('app');
